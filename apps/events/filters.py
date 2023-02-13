d6d6010725f3310336295b525a9415eb4eff8096
Fábio Rocha <rochafabio462@gmail.com>
2023-02-13 15:36:43 -0300

    ajustes

--- apps/events/filters.py
+++ apps/events/filters.py
@@ -2,7 +2,18 @@ from django_filters import FilterSet, ChoiceFilter, CharFilter
 from django import forms
 
 from . models import Event
-
+choices = (
+        ("1","04 de Outubro"),
+        ("2","Pequeno Mário"),
+        ("3","Ariamiro"),
+        ("4","Maria Dália"),
+        ("5","Baixa do Fogo"),
+        ("6","Serra"),
+        ("7","Angicos"),
+        ("8","Jerimum"),
+        ("9","Ema"),
+        ("10","Carnaubinha")
+        )
 class EventFilter(FilterSet):
 
     name = CharFilter(
@@ -10,7 +21,7 @@ class EventFilter(FilterSet):
     )
 
     school = ChoiceFilter(
-        widget=forms.Select(attrs={"class": "form-control"}),
+       choices=choices,widget=forms.Select(attrs={"class": "form-control"})
     )
 
     class Meta:
